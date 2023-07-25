b67c62e1946d1f6927963bd9ee2283467bc3c4fc
SpangorBen <netflixbenzzinbi@gmail.com>
2023-07-25 15:40:47 +0100

    first commit

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int	_putnbr(int n);
+int	_putchar(char c);
+int	_putstr(char *str);
+int _printf(const char *format, ...);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _check_format - check format of printf
+ * @args: va_list arg
+ * @c: character
+ * Return: length of the printed format
+*/
+
+int	_check_format(va_list args, char c)
+{
+	int l = 0;
+
+	switch (c)
+	{
+	case 'c':
+		l += _putchar(va_arg(args, int));
+		break;
+	case 's':
+		l += _putstr(va_arg(args, const char *));
+		break;
+	case '%':
+		l += _putchar('%');
+		break;
+	case 'd':
+		l += _putnbr(va_arg(args, int));
+		break;
+	case 'i':
+		l += _putnbr(va_arg(args, int));
+		break;
+	default:
+		break;
+	}
+	return (l);
+}
+
+/**
+ * _printf - Build out the printf function
+ * @format: string passed with possible format specifiers
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int		i = 0;
+	int		len = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] != '%')
+			len += _putchar(format[i]);
+		else if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			len += _check_format(args, format[i + 1]);
+			i++;
+		}
+		i++;
+	}
+	return (len);
+}
